0d93573bd3a3ad3635338d4bcce8d0ccaf163fbc
Tihomir Selak <tknox.dr@gmail.com>
2023-12-15 19:58:33 +0100

    BL-08: remove dist for gihub static pages

--- dist/public/assets/_...404_-25d86749.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./index-216d771d.js";const o=t("<div><h2>Oops! Couldn't find that blog post"),l=()=>o();export{l as default};
--- dist/public/assets/search-f0a685bf.js
+++
@@ -1,6 +0,0 @@
-import{u as l,f as p,k as u,c as s,i as m,t as d,s as r,l as t,n as h}from"./index-216d771d.js";import{P as f}from"./Posts-9b4a5b59.js";const g=d("<section>"),x=r("main")`
-  padding: 48px 8px;
-`,C=r("h1")`
-  text-align: center;
-  margin: 8px 0;
-`;function w(){const o=l(),[a,n]=p(o()?.posts);return u(()=>{const e=t.toLower(h()),c=t.filter(o()?.posts,i=>t.includes(t.toLower(t.get(i,["title"])),e));n(c)}),s(x,{get children(){return[s(C,{children:"Search"}),(()=>{const e=g();return m(e,s(f,{posts:a})),e})()]}})}export{w as default};
